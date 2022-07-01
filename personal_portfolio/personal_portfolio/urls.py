204e9ecefc899cc95651ba59ec0f84576c3399fe
Oscar Adimi <osadimi@microsoft.com>
2022-07-01 09:55:38 -0700

    push additional files

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #media URL settings referrencing the media url and media root in the settings file
+
