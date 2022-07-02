d7f5086720db26aebb981a21f60d4079c7d1817e
Oscar Adimi <osadimi@microsoft.com>
2022-07-02 09:05:39 -0700

    added new files

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-date')[:5] #could have used ('-date').all() to show all the blogs
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
